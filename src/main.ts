40456cab54bbcee467af73167e7f5b9f2f7df68f
whitered932 <c0d3ar@ya.ru>
2021-08-08 23:07:15 +0500

    feat: Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Visit controLL')
+    .setContact(
+      'whitered932',
+      'https://github.com/whitered932',
+      'whitered932@yandex.ru',
+    )
+    .setDescription('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
