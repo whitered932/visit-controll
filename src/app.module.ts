6f2f5871a3d201abdf68b4a19ba9c2d051718f73
whitered932 <c0d3ar@ya.ru>
2021-08-08 23:22:52 +0500

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VisitorsModule } from './visitors/visitors.module';
 import { VisitsModule } from './visits/visits.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [VisitorsModule, VisitsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    VisitorsModule,
+    VisitsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
