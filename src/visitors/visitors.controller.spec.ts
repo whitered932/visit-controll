b401e90fb4ec99a8a6a1b1f6d94159f176d17007
whitered932 <c0d3ar@ya.ru>
2021-08-08 22:45:44 +0500

    feat: added modules (visitors, visits)

---
+++ src/visitors/visitors.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { VisitorsController } from './visitors.controller';
+
+describe('VisitorsController', () => {
+  let controller: VisitorsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [VisitorsController],
+    }).compile();
+
+    controller = module.get<VisitorsController>(VisitorsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
