0a9eb740ebcfb5b1d452ca05d750b6760477baad
whitered932 <c0d3ar@ya.ru>
2021-08-09 13:33:38 +0500

    feat: Added env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,23 @@ import { AppService } from './app.service';
 import { VisitorsModule } from './visitors/visitors.module';
 import { VisitsModule } from './visits/visits.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      database: 'postgres',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        database: configService.get('DB_NAME'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
     VisitorsModule,
     VisitsModule,
